f1022b207c3d84a3b37c1dfdf636493f4cfb8941
anthony0tran <anthony0tran@gmail.com>
2024-01-29 22:09:50 +0100

    add migration for profile FetchedOn

---
+++ 4n2h0ny.Steam.API/Migrations/20240129210643_Add_Profile_FetchedOn.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _4n2h0ny.Steam.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Profile_FetchedOn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FetchedOn",
+                table: "Profiles",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FetchedOn",
+                table: "Profiles");
+        }
+    }
+}
