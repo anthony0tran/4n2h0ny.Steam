3b3e1b4c391cf74fa249ab1184b9f9b4ef618742
anthony0tran <anthony0tran@gmail.com>
2024-02-10 23:06:32 +0100

    add unit test

--- 4n2h0ny.Steam.Tests/HelpersTests.cs
+++ 4n2h0ny.Steam.Tests/HelpersTests.cs
@@ -21,5 +21,13 @@ namespace _4n2h0ny.Steam.Tests
 
             Assert.True(result == DateTime.MinValue);
         }
+
+        [Fact]
+        public void X()
+        {
+            string profileDataString = "\r\n\t\tg_rgProfileData = {\"url\":\"https:\\/\\/steamcommunity.com\\/profiles\\/76561198802957358\\/\",\"steamid\":\"76561198802957358\",\"personaname\":\"animutrix\",\"summary\":\"<img src=\\\"https:\\/\\/community.cloudflare.steamstatic.com\\/economy\\/emoticon\\/happyfroggy\\\" alt=\\\":happyfroggy:\\\" class=\\\"emoticon\\\"><br><br> <a class=\\\"bb_link\\\" href=\\\"https:\\/\\/steamcommunity.com\\/id\\/4n2h0ny\\/\\\" target=\\\"_blank\\\" rel=\\\"\\\" > <img src=\\\"https:\\/\\/community.cloudflare.steamstatic.com\\/economy\\/emoticon\\/VLOVEIT\\\" alt=\\\":VLOVEIT:\\\" class=\\\"emoticon\\\"><img src=\\\"https:\\/\\/community.cloudflare.steamstatic.com\\/economy\\/emoticon\\/VLOVEIT\\\" alt=\\\":VLOVEIT:\\\" class=\\\"emoticon\\\"><img src=\\\"https:\\/\\/community.cloudflare.steamstatic.com\\/economy\\/emoticon\\/VLOVEIT\\\" alt=\\\":VLOVEIT:\\\" class=\\\"emoticon\\\"> <\\/a> \"};\r\n\t\tconst g_bViewingOwnProfile = 0;\r\n\t\t$J( function() {\r\n\t\t\twindow.Responsive_ReparentItemsInResponsiveMode && Responsive_ReparentItemsInResponsiveMode( '.responsive_groupfriends_element', $J('#responsive_groupfriends_element_ctn') );\r\n\t\t\t\r\n\t\t\tSetupAnimateOnHoverImages();\r\n\t\t});\r\n\t";
+        
+            ProfileDataParser.ParseProfileData(profileDataString);
+        }
     }
 }
